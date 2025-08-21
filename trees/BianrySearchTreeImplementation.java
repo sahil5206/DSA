f529f5f2a47119201e8d41cf9a56c7025917d277
Sahil Gupta <Sahilg5206official@gmail.com>
2025-08-21 22:23:32 +0530

    Update BianrySearchTreeImplementation.java

--- trees/BianrySearchTreeImplementation.java
+++ trees/BianrySearchTreeImplementation.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class BianrySearchTree{
+class BST{
     static class Node{
         public int data;
         public Node left, right;
@@ -11,6 +11,7 @@ class BianrySearchTree{
             this.right = null;
         }
     }
+    
     public static Node insert(Node root, int val){
         if(root == null){
             root = new Node(val);
@@ -18,13 +19,13 @@ class BianrySearchTree{
         }
         
         if(root.data > val){
-           root.left =  insert(root.left, val);
-        }else{
-           root.right = insert(root.right, val);
+            root.left = insert(root.left, val);
+        }else if(root.data < val){
+            root.right = insert(root.right, val);
         }
-        
         return root;
     }
+    
     public static void inorder(Node root){
         if(root == null){
             return;
@@ -35,34 +36,68 @@ class BianrySearchTree{
         inorder(root.right);
     }
     
+    public static Node delete(Node root, int val){
+        if(root == null){
+            return null;
+        }
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }else if(root.data < val){
+            root.right = delete(root.right, val);
+        }else{ //root.data == val
+        //case 1
+            if(root.left == null && root.right == null){
+                return null;
+            }
+            //case 2
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+            
+            //case 3
+            Node successor = inorderSuccessor(root.right);
+            root.data = successor.data;
+            root.right = delete(root.right, successor.data);
+        }
+        return root;
+    }
+    
+    public static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+    
+    public static void printInRange(Node root, int x, int y){
+        if(root == null){
+            return;
+        }
+        if(root.data >= x && root.data <= y){
+            printInRange(root.left, x, y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, x, y);
+        }else if(root.data >= y){
+            printInRange(root.left, x, y);
+        }else{
+            printInRange(root.right, x, y);
+        }
+    }
+    
     public static void main(String[] args){
-        int[] values = {5,1,3,4,2,7};
+        int[] values = {3,1,4,2,5,7};
         Node root = null;
         
         for(int i = 0; i < values.length; i++){
-            root = insert(root, values[i]); 
+            root = insert(root, values[i]);
         }
         inorder(root);
         System.out.println();
-        
-        if(search(root, 7) != null){
-            System.out.print("founded");
-        }else{
-            System.out.print("not founded");
-        }
-        
-    }
-    
-    public static boolean search(Node root, int key){
-        if(root == null){
-            return false;
-        }
-        
-        if(root.data > key){
-            return search(root.left, key);
-        }else if(root.data < key){
-            return search(root.right, key);
-        }
-        return true;
+        delete(root,7);
+        inorder(root);
+        System.out.println();
+        printInRange(root, 2, 5);
     }
 }
