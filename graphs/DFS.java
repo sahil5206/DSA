abf3ccc32e5cf427e3711f64459ac8bb5991b642
Sahil Gupta <Sahilg5206official@gmail.com>
2025-10-04 20:37:18 +0530

    Update DFS.java

--- graphs/DFS.java
+++ graphs/DFS.java
@@ -1,3 +1,4 @@
+//This is the iterative approach and below that is the recursive approach
 import java.util.*;
 
 class Graph {
@@ -54,3 +55,58 @@ class Graph {
         g.DFSIterative(0);
     }
 }
+
+
+//below is the code for Recusrive DFS
+
+import java.util.*;
+
+class Graph {
+    private int V;
+    private LinkedList<Integer>[] adj; 
+
+    // Constructor
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    // Add edge to graph
+    void addEdge(int v, int w) {
+        adj[v].add(w); 
+    }
+
+    // Recursive DFS helper
+    private void DFSUtil(int v, boolean[] visited) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        for (int neighbor : adj[v]) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    void DFS(int start) {
+        boolean[] visited = new boolean[V];
+        DFSUtil(start, visited);
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(6);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+
+        System.out.println("Depth First Traversal starting from node 0:");
+        g.DFS(0);
+    }
+}
+
