0d1b9ba6b5f80c527c97728fc0819c80b83e634b
Sahil Gupta <Sahilg5206official@gmail.com>
2025-10-04 20:33:44 +0530

    Create DFS.java

---
+++ graphs/DFS.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Graph {
+    private int V;
+    private LinkedList<Integer>[] adj; 
+
+    Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w); 
+    }
+
+ 
+    void DFSIterative(int start) {
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(start);
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+
+            if (!visited[node]) {
+                System.out.print(node + " ");
+                visited[node] = true;
+            }
+
+            List<Integer> neighbors = adj[node];
+            for (int i = neighbors.size() - 1; i >= 0; i--) {
+                int neighbor = neighbors.get(i);
+                if (!visited[neighbor]) {
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(6);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+
+        System.out.println("Depth First Traversal (Iterative) starting from node 0:");
+        g.DFSIterative(0);
+    }
+}
